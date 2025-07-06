bd01c1e53663769b39c790e4795649849d9218c7
ramavtar07 <ramavtarsharma85507@gmail.com>
2025-07-06 18:37:48 +0530

    integrate Zustand for state management

--- client/src/pages/profile/index.jsx
+++ client/src/pages/profile/index.jsx
@@ -1,8 +1,10 @@
+import { useAppStore } from "@/store";
 
 const Profile = () => {
+  const { userInfo } = useAppStore();
   return (
     <div>
-      <h1>Profile Page</h1>
+      <h1>Email: {userInfo?.email}</h1>
     </div>
   )
 }
